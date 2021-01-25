4b027968ba5d9c3a5b2a10dfa303b1ff5615d197
Amine.mouad <amouad@umanis.com>
2021-01-25 15:43:34 +0100

    Finished about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import sanityClient from '../client';
+import imageUrlBuilder from '@sanity/image-url';
+import BlockContent from '@sanity/block-content-to-react';
+
+const builder = imageUrlBuilder(sanityClient);
+
+function urlFor(source) {
+  return builder.image(source);
+}
 
 function About() {
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "author"]{
+      name,
+      bio,
+      "authorImage": image.asset->url
+    }`
+      )
+      .then((data) => setAuthor(data[0]))
+      .catch((err) => console.error(err));
+  }, []);
+
+  if (!author) return <div>Loading...</div>;
   return (
-    <div>
-      <h1>About Page</h1>
-    </div>
+    <main className='relative '>
+      <img
+        src='https://images.unsplash.com/photo-1584890554654-b4e4c00c0842?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+        alt='sunset'
+        className='absolute w-full'
+      />
+      <div className='p-10 lg:pt-48 container mx-auto relative'>
+        <section className='bg-green-800 rounded-lg shadow-2xl lg:flex p-20'>
+          <img
+            src={urlFor(author.authorImage).url()}
+            alt={author.name}
+            className='rounded w-32 h-32 lg:w-64 lg:h-64 mr-8'
+          />
+          <div className='text-lg flex flex-col justify-center'>
+            <h1 className='cursive text-6xl text-green-300 mb-4'>
+              Hey there. I'm{' '}
+              <span className='text-green-100'>{author.name}</span>
+            </h1>
+            <div className='prose lg:prose-xl text-white'>
+              <BlockContent
+                blocks={author.bio}
+                project='epanelgn'
+                dataset='production'
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
   );
 }
 
